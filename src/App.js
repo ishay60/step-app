c9015c52eed731ed47cd34f80e6729aaa2ff1028
Ishay Rosengarten <ishay60@gmail.com>
2023-08-03 11:41:35 +0300

    changed some value changes to callback funcs

--- src/App.js
+++ src/App.js
@@ -2,46 +2,56 @@ import { useState } from "react";
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  //const [test, setTest] = useState({ name: "Ishay" });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
   }
   function handleNext() {
     if (step < 3) setStep(step + 1);
+    //setTest({ name: "fred" });
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}:{messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => handlePrevious()}
-          // onMouseEnter={() => alert("test")}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={() => handleNext()}
-        >
-          Next
-        </button>
-      </div>
+          <p className="message">
+            Step {step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => handlePrevious()}
+              // onMouseEnter={() => alert("test")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => handleNext()}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 const messages = [
   "Learn react😊",
-  "Apply for jobs💼",
+  "Apply for jobs💼 ",
   "Invest your new income😊",
 ];
